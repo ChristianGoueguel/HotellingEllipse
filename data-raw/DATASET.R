60b29be4ca6708922f961d99f2137a898cb3517e
ChristianGoueguel <christian.goueguel@gmail.com>
2021-05-12 23:23:38 -0400

    Updates

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,7 +1,6 @@
-## code to prepare `DATASET` dataset goes here
 specData <-
   data.table::fread("~/Documents/Laserag/R&D projects/Soil/Eurofins/Texture/processed data/Data_10mJ/data_avg.csv") %>%
   select(-c(Group.1:Silt)) %>%
   select(-c(`199.3771616`:`240.0751218`))
 
-usethis::use_data(DATASET, overwrite = TRUE)
+usethis::use_data(specData, overwrite = TRUE)
