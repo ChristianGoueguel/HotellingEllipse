db9687ca150104217765e190258ed013adace8fb
ChristianGoueguel <christian.goueguel@gmail.com>
2021-05-12 23:44:20 -0400

    Updates in the dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,2 @@
-specData <-
-  data.table::fread("~/Documents/Laserag/R&D projects/Soil/Eurofins/Texture/processed data/Data_10mJ/data_avg.csv") %>%
-  dplyr::select(-c(Group.1:Silt)) %>%
-  dplyr::select(-c(`199.3771616`:`240.0751218`))
-
-colnames(specData) <- paste("X", 1:6669, sep = "")
-
+specData <- data.table::fread("~/Documents/Laserag/R&D projects/Soil/Eurofins/Texture/processed data/Data_10mJ/specData.csv")
 usethis::use_data(specData, overwrite = TRUE)
