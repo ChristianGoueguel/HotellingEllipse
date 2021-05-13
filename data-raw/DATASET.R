52e5407252705a1ba3beb7b58b1d78da6d12e061
ChristianGoueguel <christian.goueguel@gmail.com>
2021-05-12 23:37:21 -0400

    Update in the dataset code, column names

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,8 @@
 specData <-
   data.table::fread("~/Documents/Laserag/R&D projects/Soil/Eurofins/Texture/processed data/Data_10mJ/data_avg.csv") %>%
-  select(-c(Group.1:Silt)) %>%
-  select(-c(`199.3771616`:`240.0751218`))
+  dplyr::select(-c(Group.1:Silt)) %>%
+  dplyr::select(-c(`199.3771616`:`240.0751218`))
+
+colnames(specData) <- paste("X", 1:6669, sep = "")
 
 usethis::use_data(specData, overwrite = TRUE)
