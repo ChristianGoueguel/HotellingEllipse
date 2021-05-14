f8e67efe0cd4c13bb20f382225e982545c17711c
ChristianGoueguel <christian.goueguel@gmail.com>
2021-05-14 16:08:35 -0400

    Updates tests

--- tests/testthat/test-test_fun.R
+++ tests/testthat/test-test_fun.R
@@ -1,26 +1,33 @@
 test_that("function stops when it should", {
-
   expect_error(ellipseParam(data = 0, k = 0, pcx = 0, pcy = 0))
-  expect_error(ellipseCoord(data = 0, pcx = 0, pcy = 0, conf.limit = 0.95, pts = 200))
+  expect_error(ellipseCoord(data = 0, pcx = 0, pcy = 0, conf.limit = 0, pts = 0))
+})
 
-  })
-
-test_that("ellipseParam and ellipseCoord functions", {
-
-  library(dplyr)
-
-  set.seed(123)
-  pca_mod <- specData %>%
+library(dplyr)
+set.seed(123)
+pca_mod <- specData %>%
   select(where(is.numeric)) %>%
   FactoMineR::PCA(scale.unit = FALSE, graph = FALSE)
 
-  pca_scores <- pca_mod %>%
-    purrr::pluck("ind", "coord") %>%
-    tibble::as_tibble()
+pca_scores <- pca_mod %>%
+  purrr::pluck("ind", "coord") %>%
+  tibble::as_tibble()
 
-  res_t2 <- ellipseParam(data = pca_scores, k = 2, pcx = 1, pcy = 2)
-  xy_coord <- ellipseCoord(data = pca_scores, pcx = 1, pcy = 2, conf.limit = 0.95, pts = 200)
+res_t2 <- ellipseParam(data = pca_scores, k = 2, pcx = 1, pcy = 2)
+xy_coord <- ellipseCoord(data = pca_scores, pcx = 1, pcy = 2, conf.limit = 0.95, pts = 200)
 
+
+test_that("input errors", {
+  expect_error(ellipseParam(data = pca_scores, pcx = 1, pcy = 1), "Please provide two different components in pcx and pcy.")
+  expect_error(ellipseParam(data = pca_scores, pcx = 0, pcy = 1), "No component is provided either in pcx or pcy, or both.")
+  expect_error(ellipseCoord(data = pca_scores, pcx = 0, pcy = 1), "No component is provided either in pcx or pcy, or both.")
+  expect_error(ellipseParam(data = pca_scores, k = 1), "k must be at least equal to 2.")
+  expect_error(ellipseParam(data = pca_scores, k = ncol(pca_scores)+1), "k exceeds the number of component in the data.")
+  expect_error(ellipseCoord(data = pca_scores, pcx = 0, pcy = 1), "No component is provided either in pcx or pcy, or both.")
+  expect_error(ellipseCoord(data = pca_scores, conf.limit = 2), "Confidence level should be between 0 and 1")
+})
+
+test_that("ellipseParam and ellipseCoord functions", {
   expect_type(res_t2, "list")
   expect_type(xy_coord, "list")
   expect_named(res_t2, c("Tsquared", "Ellipse", "cutoff.99pct", "cutoff.95pct"), ignore.order = TRUE, ignore.case = TRUE)
