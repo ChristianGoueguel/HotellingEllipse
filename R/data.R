efd5d888bb1546e68e6f36b437ee7bbb832ace0d
ChristianGoueguel <christian.goueguel@gmail.com>
2021-05-14 18:44:49 -0400

    updates

--- R/data.R
+++ R/data.R
@@ -3,5 +3,5 @@
 #' A dataset containing the emission spectra of 171 soils measured in laboratory.
 #'
 #' @format A data frame with 171 rows and 6670 variables (wavelengths).
-#' @source \url{https://doi.org/10.1039/C9JA00090A}
+#' @source \doi{10.1039/C9JA00090A}
 "specData"
