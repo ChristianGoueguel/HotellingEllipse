ad169d89e0642214749d9346236d6d8100549556
ChristianGoueguel <christian.goueguel@gmail.com>
2022-02-23 13:27:39 -0700

    Updates

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
-#' Laser-induced breakdown spectroscopy (LIBS) spectra of 171 soil samples
-#'
-#' A dataset containing the emission spectra of 171 soils measured in laboratory.
-#'
-#' @format A data frame with 171 rows and 6670 variables (wavelengths).
+#' @title LIBS spectra of 171 soil samples
+#' @description A dataset containing the emission spectra of 171 soils measured in laboratory conditions.
+#' The samples were cleaned, dried, homogenized, sieved (10 Mesh size) and thereafter pelletized prior to LIBS measurements.
+#' LIBS spectra were preprocessed by performing baseline removal.
+#' @format Data frame of 171 rows (soil samples) and 6670 columns (wavelengths).
 #' @source \doi{10.1039/C9JA00090A}
 "specData"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,7 @@
 ## code to prepare `specData` dataset goes here
-specData <- data.table::fread("~/Documents/Laserag/R&D projects/Soil/Eurofins/Texture/processed data/Data_10mJ/specData.csv")
+specData <-
+  data.table::fread("~/Documents/Laserag/R&D projects/Soil/Eurofins/Texture/processed data/Data_10mJ/specDatafull.csv") %>%
+  dplyr::select(-c(V1:V482))
+
 usethis::use_data(specData, overwrite = TRUE)
+
