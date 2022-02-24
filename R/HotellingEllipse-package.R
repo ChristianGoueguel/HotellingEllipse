572d80a3336e3b4ceba94b010586de91e563e2ca
ChristianGoueguel <christian.goueguel@gmail.com>
2022-02-23 20:32:27 -0700

    Updates

---
+++ R/HotellingEllipse-package.R
@@ -0,0 +1,25 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr select
+#' @importFrom FactoMineR PCA
+#' @importFrom ggforce geom_ellipse
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 annotate
+#' @importFrom ggplot2 element_blank
+#' @importFrom ggplot2 geom_hline
+#' @importFrom ggplot2 geom_point
+#' @importFrom ggplot2 geom_segment
+#' @importFrom ggplot2 geom_vline
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 scale_fill_gradient
+#' @importFrom ggplot2 scale_fill_viridis_c
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 theme_bw
+#' @importFrom ggplot2 theme_grey
+#' @importFrom lifecycle badge
+#' @importFrom purrr pluck
+## usethis namespace: end
+NULL
