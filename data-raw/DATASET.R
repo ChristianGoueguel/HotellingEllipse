9ad828efcc7b8078c8201c929f4644eec04e0076
ChristianGoueguel <christian.goueguel@gmail.com>
2022-06-23 14:35:19 -0700

    Update

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,7 +1,7 @@
 ## code to prepare `specData` dataset goes here
 specData <-
   data.table::fread("~/Documents/Laserag/R&D projects/Soil/Eurofins/Texture/processed data/Data_10mJ/specDatafull.csv") %>%
-  dplyr::select(-c(V1:V482))
+  dplyr::select(-c(V1:V1000))
 
 usethis::use_data(specData, overwrite = TRUE, compress = "xz")
 
