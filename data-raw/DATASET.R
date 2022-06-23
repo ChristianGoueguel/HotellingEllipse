d6b78a809a7058628a544d6644c9c66bc8d5982f
ChristianGoueguel <christian.goueguel@gmail.com>
2022-06-23 15:38:29 -0700

    Update: LIBS spectra

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,6 @@
 ## code to prepare `specData` dataset goes here
 specData <-
-  data.table::fread("~/Documents/Laserag/R&D projects/Soil/Eurofins/Texture/processed data/Data_10mJ/specDatafull.csv") %>%
+  data.table::fread("~/Documents/Laserag/R&D projects/specData.csv") %>%
   dplyr::select(-c(V1:V1000))
 
 usethis::use_data(specData, overwrite = TRUE, compress = "xz")
