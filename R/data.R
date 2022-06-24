393db4dd9886050b2b0c0b23284f2f04e91ceae9
ChristianGoueguel <christian.goueguel@gmail.com>
2022-06-24 11:32:18 -0700

    Update `specData`

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #' @description A dataset containing the emission spectra of 171 soils measured in laboratory conditions.
 #' The samples were cleaned, dried, homogenized, sieved (10 Mesh size) and thereafter pelletized prior to LIBS measurements.
 #' LIBS spectra were preprocessed by performing baseline removal.
-#' @format Data frame of 100 rows (soil samples) and 5652 columns (wavelengths).
+#' @format Data frame of 100 rows (soil samples) and 3152 columns (wavelengths).
 #' @source \doi{10.1039/C9JA00090A}
 "specData"
 
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,6 @@
 ## code to prepare `specData` dataset goes here
 specData <-
   data.table::fread("~/Documents/Laserag/R&D projects/specData.csv") %>%
-  dplyr::select(-c(V1:V2000))
+  dplyr::select(-c(V1:V4000))
 
 usethis::use_data(specData, overwrite = TRUE, compress = "xz")
