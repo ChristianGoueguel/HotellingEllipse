acc3cc27484412e60616319c6ab1c9da32051848
ChristianGoueguel <christian.goueguel@gmail.com>
2024-04-25 18:28:30 -0400

    Minor correction

--- R/ellipseParam.R
+++ R/ellipseParam.R
@@ -46,7 +46,7 @@ ellipseParam <- function(data, k = 2, pcx = 1, pcy = 2) {
     stop("k must be at least equal to 2.")
   }
   if (k > ncol(data)) {
-    stop("k exceeds the number of columns in the data.")
+    stop("k exceeds the number of component in the data.")
   }
   X <- as.matrix(data)
   n <- nrow(X)
